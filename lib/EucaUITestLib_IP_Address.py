dcf6ca284dd11a23beb98c845619e262d24b9acd
root <root@eucahost-51-100.eucalyptus>
2013-06-11 00:37:09 -0700

    Refactored IP Address Lib to use the base lib functions

--- lib/EucaUITestLib_IP_Address.py
+++ lib/EucaUITestLib_IP_Address.py
@@ -3,110 +3,41 @@ from se34euca.lib.EucaUITestLib_Base import *
 class EucaUITestLib_IP_Address(EucaUITestLib_Base):
 
     def test_ui_allocate_two_ip_addresses(self):
+	print
 	print "Started Test: Allocate Two IP Addresses"
-        driver = self.driver
-        for i in range(self.retry):
-            try:
-                if self.is_element_present(By.ID, "dashboard-netsec-eip"): break
-            except: pass
-            time.sleep(1)
-        else:
-	    print "Failed Test: Allocate Two IP Addresses"
-            raise
-            return 1
-        print "Test: Go to the Page IP Address"
-        driver.find_element_by_id("dashboard-netsec-eip").click()
-        for i in range(self.retry):
-            try:
-                if self.is_element_present(By.ID, "table-eips-new"): break
-            except: pass
-            time.sleep(1)
-        else:
-	    print "Failed Test: Allocate Two IP Addresses"
-            raise
-            return 1
-        driver.find_element_by_id("table-eips-new").click()
-        for i in range(self.retry):
-            try:
-                if self.is_element_present(By.ID, "eip-allocate-count"): break
-            except: pass
-            time.sleep(1)
-        else:
-	    print "Failed Test: Allocate Two IP Addresses"
-            raise
-            return 1
+	self.verify_element_by_id("dashboard-netsec-eip")
+        print
+	print "Test: Go to the Page IP Address"
+	self.click_element_by_id("dashboard-netsec-eip")
+        self.click_element_by_id("table-eips-new")
+	self.verify_element_by_id("eip-allocate-count")
+	print
 	print "Test: Allocate Two IP Addresses"
-        driver.find_element_by_id("eip-allocate-count").clear()
-        driver.find_element_by_id("eip-allocate-count").send_keys("2")
-        for i in range(self.retry):
-            try:
-                if self.is_element_present(By.ID, "eip-allocate-btn"): break
-            except: pass
-            time.sleep(1)
-        else:
-	    print "Failed Test: Allocate Two IP Addresses"
-            raise
-            return 1
 	try:
-            driver.find_element_by_id("eip-allocate-btn").click()
+            self.driver.find_element_by_id("eip-allocate-count").clear()
+            self.driver.find_element_by_id("eip-allocate-count").send_keys("2")
 	except:
-	    print "Failed Test: Allocate Two IP Addresses"
+            print "Failed Test: Allocate Two IP Addresses"
             raise
             return 1
+	self.click_element_by_id("eip-allocate-btn")
+	print
 	print "Finished: Allocate Two IP Addresses"
 	print
 	return 0
 
     def test_ui_release_ip_address(self):
+	print
 	print "Started Test: Release IP Address"
-        driver = self.driver
-        for i in range(self.retry):
-            try:
-                if self.is_element_present(By.LINK_TEXT, "Launch new instance"): break
-            except: pass
-            time.sleep(1)
-        else:
-	    print "Failed Test: Release Two IP Addresses"
-            raise
-            return 1
-        print "Test: Go to the Page IP Address"
-        driver.find_element_by_css_selector("#dashboard-netsec-eip > span").click()
-        for i in range(self.retry):
-            try:
-                if self.is_element_present(By.ID, "table-eips-new"): break
-            except: pass
-            time.sleep(1)
-        else:
-	    print "Failed Test: Release Two IP Addresses"
-            raise
-            return 1
-        driver.find_element_by_css_selector("td.checkbox-cell.sorting_1 > input[type=\"checkbox\"]").click()
-        for i in range(self.retry):
-            try:
-                if self.is_element_present(By.ID, "more-actions-eips"): break
-            except: pass
-            time.sleep(1)
-        else:
-	    print "Failed Test: Release Two IP Addresses"
-            raise
-            return 1
-        driver.find_element_by_id("more-actions-eips").click()
+	self.verify_element_by_link_text("Launch new instance")
+        print
+	print "Test: Go to the Page IP Address"
+	self.click_element_by_css_selector("#dashboard-netsec-eip > span")
+        self.click_element_by_css_selector("td.checkbox-cell.sorting_1 > input[type=\"checkbox\"]")
+        self.click_element_by_id("more-actions-eips")
 	self.click_element_by_link_text("Release to cloud")
-        for i in range(self.retry):
-            try:
-                if self.is_element_present(By.ID, "btn-eips-release-release"): break
-            except: pass
-            time.sleep(1)
-        else:
-	    print "Failed Test: Release Two IP Addresses"
-            raise
-            return 1
-	try:
-            driver.find_element_by_id("btn-eips-release-release").click()
-	except:
-	    print "Failed Test: Release Two IP Addresses"
-            raise
-            return 1
+        self.click_element_by_id("btn-eips-release-release")
+	print
 	print "Finished: Release IP Address" 
 	print
 	return 0
