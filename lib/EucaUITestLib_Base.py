cc597d506d721eaba94573851f2c6d8b423b8857
root <root@eucahost-51-100.eucalyptus>
2013-06-11 12:22:34 -0700

    Refactored Base Class to use the base lib functions

--- lib/EucaUITestLib_Base.py
+++ lib/EucaUITestLib_Base.py
@@ -46,6 +46,7 @@ class EucaUITestLib_Base(unittest.TestCase):
 	print
 
     def setUp(self):
+	print
 	print "=== setUp ==="
 	this_ui = "https://" + self.ui_ip + ":" + self.port
 	this_selenium_server_url = "http://" + self.selenium_server_ip + ":" + self.selenium_server_port + "/wd/hub"
@@ -58,202 +59,112 @@ class EucaUITestLib_Base(unittest.TestCase):
         self.driver.implicitly_wait(30)
         self.base_url = this_ui
         self.verificationErrors = []
+	print
 	print "Started Selenium Test Targeted at : " + self.base_url
 	print
 
     def tearDown(self):
+	print
 	print "=== tearDown ==="
 	self.driver.quit()
         self.assertEqual([], self.verificationErrors)
+	print
 	print "Finished Selenium Test Targeted at : " + self.base_url
 	print    
 
     def is_element_present(self, how, what):
-        try: self.driver.find_element(by=how, value=what)
-        except NoSuchElementException, e: return False
+        try:
+	    self.driver.find_element(by=how, value=what)
+        except NoSuchElementException, e:
+	    return False
         return True
 
 
     def test_ui_login(self):
+	print
 	print "Started Test: Login"	
-        driver = self.driver
-        driver.get(self.base_url + "/")
-	print "Test: Received the Page Title -> " + driver.title
-        for i in range(self.retry):
-            try:
-                if self.is_element_present(By.ID, "login"): break
-            except: pass
-            time.sleep(1)
-        else:
-	    print "Failed Test: Login"
-	    raise
-	    return 1
+        self.driver.get(self.base_url + "/")
+	print
+	print "Test: Received the Page Title -> " + self.driver.title
+	self.verify_element_by_id("login")
+	print
 	print "Test: Received the Login Page"
-        driver.find_element_by_id("account").clear()
-        driver.find_element_by_id("account").send_keys(self.accountname)
-        driver.find_element_by_id("username").clear()
-        driver.find_element_by_id("username").send_keys(self.username)
-        driver.find_element_by_id("password").clear()
-        driver.find_element_by_id("password").send_keys(self.password)
-        driver.find_element_by_name("login").click()
+        self.driver.find_element_by_id("account").clear()
+        self.driver.find_element_by_id("account").send_keys(self.accountname)
+        self.driver.find_element_by_id("username").clear()
+        self.driver.find_element_by_id("username").send_keys(self.username)
+        self.driver.find_element_by_id("password").clear()
+        self.driver.find_element_by_id("password").send_keys(self.password)
+	print
 	print "Test: Typed the User Info and Clicked the Login Button"
-        for i in range(self.retry):
-            try:
-                if self.is_element_present(By.LINK_TEXT, "Launch new instance"): break
-            except: pass
-            time.sleep(1)
-        else:
-	    print "Failed Test: Login"
-	    raise
-	    return 1
+	self.click_element_by_name("login")
+	self.verify_element_by_link_text("Launch new instance")
 	print "Finished Test: Login"
 	print
 	return 0
 
     def test_ui_logout(self):
+	print
 	print "Started Test: Logout"
-        driver = self.driver
-        for i in range(self.retry):
-            try:
-                if self.is_element_present(By.ID, "euca-logo"): break
-            except: pass
-            time.sleep(1)
-        else:
-	    print "Failed Test: Logout"
-	    raise
-	    return 1
-	driver.find_element_by_id("euca-logo").click()
-	print "Test: Received the Page Title -> " + driver.title
-        for i in range(self.retry):
-            try:
-                if self.is_element_present(By.LINK_TEXT, "Launch new instance"): break
-            except: pass
-            time.sleep(1)
-        else:
-	    print "Failed Test: Logout"
-	    raise
-	    return 1
+	self.click_element_by_id("euca-logo")
+	print
+	print "Test: Received the Page Title -> " + self.driver.title
+	self.verify_element_by_link_text("Launch new instance")
 	this_link = self.username + "@" + self.accountname
-	for i in range(self.retry):
-            try:
-                if self.is_element_present(By.LINK_TEXT, this_link): break
-            except: pass
-            time.sleep(1)
-        else:
-	    print "Failed Test: Logout"
-	    raise
-	    return 1
-	print "Test: Clicking User Account Menu " + this_link
-        driver.find_element_by_link_text(this_link).click()
 	print "Test: Clicked User Account Menu " + this_link
-	for i in range(self.retry):
-            try:
-                if self.is_element_present(By.LINK_TEXT, "Log out"): break
-            except: pass
-	    time.sleep(1)
-        else:
-	    print "Failed Test: Logout"
-            raise
-            return 1
-	print "Test: Clicking Log out"
-	driver.find_element_by_link_text("Log out").click()
+	self.click_element_by_link_text(this_link)
 	print "Test: Clicked the Logout Button"
-        for i in range(self.retry):
-            try:
-                if self.is_element_present(By.ID, "login"): break
-            except: pass
-            time.sleep(1)
-        else:
-	    print "Failed Test: Logout"
-            raise
-            return 1
+	self.click_element_by_link_text("Log out")
+        self.verify_element_by_id("login")
+	print
 	print "Finished Test: Logout"
 	print
 	return 0
 
 
     def test_ui_gotopage_dashboard(self):
+	print
 	print "Started Test: GotoPage Dashboard"
-        driver = self.driver
-        for i in range(self.retry):
-            try:
-                if self.is_element_present(By.LINK_TEXT, "Dashboard"): break
-            except: pass
-            time.sleep(1)
-        else:
-	    print "Failed Test: GotoPage Dashboard"
-            raise
-	    return 1
-	driver.find_element_by_link_text("Dashboard").click()
-	print "Test: Received the Page Title -> " + driver.title
-        for i in range(self.retry):
-            try:
-                if self.is_element_present(By.LINK_TEXT, "Launch new instance"): break
-            except: pass
-            time.sleep(1)
-        else:
-	    print "Failed Test: GotoPage Dashboard"
-            raise
-	    return 1
-        try: self.assertTrue(self.is_element_present(By.LINK_TEXT, "Launch new instance"))
-        except AssertionError as e: self.verificationErrors.append(str(e))
+	self.click_element_by_link_text("Dashboard")
+	print
+	print "Test: Received the Page Title -> " + self.driver.title
+	self.verify_element_by_link_text("Launch new instance")
+	print
 	print "Finished Test: GotoPage Dashboard"
 	print
 	return 0
 
 
     def test_ui_view_page_get_dashboard_source(self):
+	print
 	print "Started Test: View Page Get Dashboard Source"
 	print
-        driver = self.driver
-	for i in range(self.retry):
-            try:
-                if self.is_element_present(By.ID, "euca-logo"): break
-            except: pass
-            time.sleep(1)
-        else:
-            print "Failed Test: GotoPage Dashboard Source"
-	    raise
-            return 1
-        driver.find_element_by_id("euca-logo").click()
-        print "Test: Received the Page Title -> " + driver.title
-        for i in range(self.retry):
-            try:
-                if self.is_element_present(By.LINK_TEXT, "Launch new instance"): break
-            except: pass
-            time.sleep(1)
-        else:
-            print "Failed Test: GotoPage Dashboard Source"
-            raise
-	    return 1
+	self.click_element_by_id("euca-logo")
+        print
+	print "Test: Received the Page Title -> " + self.driver.title
+	self.verify_element_by_link_text("Launch new instance")
 	time.sleep(3)
 	try:
-	    running_instances = driver.find_element_by_css_selector("div.status-readout")
-	    running_instances_text = running_instances.text
-	    print "[DASHBOARD] Running Instances: " + running_instances_text
-	    stopped_instances = driver.find_element_by_id("dashboard-instance-stopped")
-	    stopped_instances_text = stopped_instances.text
-	    print "[DASHBOARD] Stopped Instances: " + stopped_instances_text
-	    volumes = driver.find_element_by_id("dashboard-storage-volume")
-	    volumes_text = volumes.text
-	    print "[DASHBOARD] Volumes: " + volumes_text
-	    snapshots = driver.find_element_by_id("dashboard-storage-snapshot")
-	    snapshots_text = snapshots.text
-	    print "[DASHBOARD] Snapshots: " + snapshots_text
-	    sgroup = driver.find_element_by_id("dashboard-netsec-sgroup")
-	    sgroup_text = sgroup.text
-	    print "[DASHBOARD] Security Groups: " + sgroup_text
-	    keypairs = driver.find_element_by_id("dashboard-netsec-keypair")
-	    keypairs_text = keypairs.text
-	    print "[DASHBOARD] Keypairs: " + keypairs_text
-	    eip = driver.find_element_by_id("dashboard-netsec-eip")
-	    eip_text = eip.text
-	    print "[DASHBOARD] IP Addresses: " + eip_text
+	    running_instances = self.driver.find_element_by_css_selector("div.status-readout")
+	    print "[DASHBOARD] Running Instances: " + running_instances.text
+	    stopped_instances = self.driver.find_element_by_id("dashboard-instance-stopped")
+	    print "[DASHBOARD] Stopped Instances: " + stopped_instances.text
+	    volumes = self.driver.find_element_by_id("dashboard-storage-volume")
+	    print "[DASHBOARD] Volumes: " + volumes.text
+	    snapshots = self.driver.find_element_by_id("dashboard-storage-snapshot")
+	    print "[DASHBOARD] Snapshots: " + snapshots.text
+	    sgroup = self.driver.find_element_by_id("dashboard-netsec-sgroup")
+	    print "[DASHBOARD] Security Groups: " + sgroup.text
+	    keypairs = self.driver.find_element_by_id("dashboard-netsec-keypair")
+	    print "[DASHBOARD] Keypairs: " + keypairs.text
+	    eip = self.driver.find_element_by_id("dashboard-netsec-eip")
+	    print "[DASHBOARD] IP Addresses: " + eip.text
 	    print
 	except:
 	    print "Failed Test: GotoPage Dashboard Source"
             raise
             return 1
+	print
 	print "Finished Test: View Page Get Dashboard Source"
 	print
 	return 0
@@ -270,6 +181,8 @@ class EucaUITestLib_Base(unittest.TestCase):
 	    this_element_type = By.CSS_SELECTOR
 	elif( element_type is "XPATH" ):
 	    this_element_type = By.XPATH
+	elif( element_type is "NAME" ):
+	    this_element_type = By.NAME
 
 	for i in range(self.retry):
             print "Wait On:: Trial: " + str(i)  + " Element Type: " + element_type + ", Element: " + element
@@ -303,6 +216,9 @@ class EucaUITestLib_Base(unittest.TestCase):
 
     def verify_element_by_xpath(self, element):
 	return self.check_if_element_present_by_type("XPATH", element)
+    
+    def verify_element_by_name(self, element):
+	return self.check_if_element_present_by_type("NAME", element)
 
 
     def click_element_by_link_text(self, link_text):
@@ -331,6 +247,13 @@ class EucaUITestLib_Base(unittest.TestCase):
 	    return 1
         print "Click: Element Type: XPATH, Element: "+ xpath
         self.driver.find_element_by_xpath(xpath).click()
+	return 0
+    
+    def click_element_by_name(self, name):
+	if( self.check_if_element_present_by_type("NAME", name) is not 0 ):
+	    return 1
+        print "Click: Element Type: NAME, Element: "+ name
+        self.driver.find_element_by_name(name).click()
 	return 0
 
 
