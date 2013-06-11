22332d246f6889f354017392539b8764365c7ca7
root <root@eucahost-51-100.eucalyptus>
2013-06-11 00:41:25 -0700

    Refactored Image Lib to use the base lib functions

--- lib/EucaUITestLib_Image.py
+++ lib/EucaUITestLib_Image.py
@@ -3,44 +3,15 @@ from se34euca.lib.EucaUITestLib_Base import *
 class EucaUITestLib_Image(EucaUITestLib_Base):
 
     def test_ui_gotopage_images(self):
+	print
 	print "Started Test: GotoPage Images"
-        driver = self.driver
-        for i in range(self.retry):
-            try:
-                if self.is_element_present(By.ID, "euca-logo"): break
-            except: pass
-            time.sleep(1)
-        else:
-	    print "Failed Test: GotoPage Images"
-            raise
-	    return 1
-        driver.find_element_by_id("euca-logo").click()
-	print "Test: Received the Page Title -> " + driver.title
-        for i in range(self.retry):
-            try:
-                if self.is_element_present(By.LINK_TEXT, "Images"): break
-            except: pass
-            time.sleep(1)
-        else:
-	    print "Failed Test: GotoPage Images"
-            raise
-	    return 1
-        driver.find_element_by_link_text("Images").click()
+	self.click_element_by_id("euca-logo")
+	print "Test: Received the Page Title -> " + self.driver.title
+	self.click_element_by_link_text("Images")
+	print
 	print "Test: Clicked the GotoPage Images"
-        for i in range(self.retry):
-            try:
-                if self.is_element_present(By.LINK_TEXT, "More actions"): break
-            except: pass
-            time.sleep(1)
-        else:
-	    print "Failed Test: GotoPage Images"
-            raise
-	    return 1
-	try:
-	    driver.find_element_by_link_text("More actions").click()
-	except:
-	    print "Failed Test: GotoPage Images"
-	    return 1
+	self.verify_element_by_link_text("More actions")
+	print
 	print "Finished Test: GotoPage Images"
 	print 
 	return 0
