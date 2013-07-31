b93f861ddd48248d4afbbb643d31b71279643ed7
dkavanagh <dkavanagh@gmail.com>
2013-07-31 10:57:29 -0400

    refactored runtest code

---
+++ se34euca/__init__.py
@@ -0,0 +1,90 @@
+
+import unittest, time, re
+from optparse import OptionParser
+from unittest import TestResult
+from se34euca.testcase.testcase_snapshot import *
+
+class TestRunner(object):
+    testcase = None
+    testclass = None
+    testcases = ''
+
+    def __init__(self):
+        self.selenium_server_ip = "localhost"
+        self.selenium_server_port = "4444"
+        self.ui_ip = "localhost"
+        self.port = "8888"
+        self.accountname = "eucalyptus"
+        self.username = "admin"
+        self.password = "password"
+	
+        parser = OptionParser()
+        parser.add_option("-s", "--sel_server_ip", dest="selenium_server_ip", help="selenium server ip")
+        parser.add_option("-r", "--sel_server_port", dest="selenium_server_port", help="selenium server port")
+        parser.add_option("-i", "--ip", dest="ui_ip", help="ui ip")
+        parser.add_option("-p", "--port", dest="port", help="port")
+        parser.add_option("-a", "--account", dest="accountname", help="accountname")
+        parser.add_option("-u", "--user", dest="username", help="username")
+        parser.add_option("-w", "--password", dest="password", help="password")
+        parser.add_option("-t", "--testcase", dest="testcase", help="testcase: "+self.testcases)
+        (options, args) = parser.parse_args()
+
+        if options.selenium_server_ip is not None:
+            self.selenium_server_ip = options.selenium_server_ip
+
+        if options.selenium_server_port is not None:
+            self.selenium_server_port = options.selenium_server_port
+
+        if options.ui_ip is not None:
+            self.ui_ip = options.ui_ip
+
+        if options.port is not None:
+            self.port = options.port
+
+        if options.accountname is not None:
+            self.accountname = options.accountname
+
+        if options.username is not None:
+            self.username = options.username
+
+        if options.password is not None:
+            self.password = options.password
+
+        if options.testcase is not None:
+            self.testcase = options.testcase
+
+    def start_test(self):
+        print "=============================="
+        print "TEST "+self.testclass.__name__
+        print "=============================="
+
+        testresult = TestResult()
+        ui = self.testclass(self.testcase)
+
+        print
+        print "### SETUP"
+        print "TESTCASE: " + self.testcase
+        print
+        ui.setSeleniumServerInfo(self.selenium_server_ip, self.selenium_server_port)
+        ui.setUIInfo(self.ui_ip, self.port)
+        ui.setUserInfo(self.accountname, self.username, self.password)
+
+        print
+        print "### TEST"
+        ui.run(testresult)
+
+        print
+        print "### RESULT"
+        print "Failures: "  + str(len(testresult.failures))
+        if len(testresult.failures) > 0:
+            print testresult.failures
+        print "Errors: " + str(len(testresult.errors))
+        if len(testresult.errors) > 0:
+            print testresult.errors
+
+        print
+        print "=============================="
+        print "END OF TEST : "+self.testclass.__name__
+        print "=============================="
+        exit(len(testresult.failures) + len(testresult.errors))
+
--- se34euca/runtest_simulate_user.py
+++ se34euca/runtest_simulate_user.py
@@ -1,91 +1,12 @@
 #!/usr/bin/python
 
-import unittest, time, re
-from optparse import OptionParser
-from unittest import TestResult
-from se34euca.testcase.testcase_simulate_user import *
+import se34euca
+from se34euca.testcase.testcase_simulate_user import testcase_simulate_user
 
-def main():
-
+class SimulateUser(se34euca.TestRunner):
 	testcase = "simulate_user_case_00"
-
-	selenium_server_ip = "localhost"
-	selenium_server_port = "4444"
-	ui_ip = "localhost"
-	port = "8888"
-	accountname = "eucalyptus"
-	username = "admin"
-	password = "password"
-	
-	print "=============================="
-	print "TEST SIMULATE USER"
-	print "=============================="
-
-	parser = OptionParser()
-	parser.add_option("-s", "--sel_server_ip", dest="selenium_server_ip", help="selenium server ip")
-	parser.add_option("-r", "--sel_server_port", dest="selenium_server_port", help="selenium server port")
-	parser.add_option("-i", "--ip", dest="ui_ip", help="ui ip")
-	parser.add_option("-p", "--port", dest="port", help="port")
-	parser.add_option("-a", "--account", dest="accountname", help="accountname")
-	parser.add_option("-u", "--user", dest="username", help="username")
-	parser.add_option("-w", "--password", dest="password", help="password")
-	parser.add_option("-t", "--testcase", dest="testcase", help="testcase: simulate_user_case_00, simulate_user_case_00_in_loop")
-	(options, args) = parser.parse_args()	
-
-	if options.selenium_server_ip is not None:
-		selenium_server_ip = options.selenium_server_ip
-
-	if options.selenium_server_port is not None:
-		selenium_server_port = options.selenium_server_port
-
-	if options.ui_ip is not None:
-		ui_ip = options.ui_ip
-
-	if options.port is not None:
-		port = options.port
-
-	if options.accountname is not None:
-		accountname = options.accountname
-
-	if options.username is not None:
-		username = options.username
-
-	if options.password is not None:
-		password = options.password
-
-	if options.testcase is not None:
-		testcase = options.testcase
-
-	testresult = TestResult()
-	ui = testcase_simulate_user(testcase)
-
-	print
-	print "### SETUP"
-	print "TESTCASE: " + testcase
-	print
-	ui.setSeleniumServerInfo(selenium_server_ip, selenium_server_port)
-	ui.setUIInfo(ui_ip, port)
-	ui.setUserInfo(accountname, username, password)
-	
-	print
-	print "### TEST"
-	ui.run(testresult)
-
-	print
-	print "### RESULT"
-	print "Failures: "  + str(len(testresult.failures))
-	if len(testresult.failures) > 0:
-		print testresult.failures
-	print "Errors: " + str(len(testresult.errors))
-	if len(testresult.errors) > 0:
-		print testresult.errors
-
-	print
-	print "=============================="
-	print "END OF TEST : SIMULATE USER"
-	print "=============================="
-
+	testclass = testcase_simulate_user
 
 if __name__ == "__main__":
-    main()
-    exit
+    SimulateUser().start_test()
+    
