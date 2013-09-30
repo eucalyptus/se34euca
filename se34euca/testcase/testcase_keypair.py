720a140c1c0031fab7c41f90df2ae9d87bd0e6e9
Kyo Lee <kyolee310@gmail.com>
2013-09-30 15:11:04 -0700

    Added verification steps to the keypair test cases

--- se34euca/testcase/testcase_keypair.py
+++ se34euca/testcase/testcase_keypair.py
@@ -18,6 +18,7 @@ class testcase_keypair(testcase_base):
         print "=== runTest: Generate Keypair ==="
         self.eucaUITester.base.test_ui_login()
         self.eucaUITester.keypair.test_ui_import_keypair()
+        self.eucaUITester.keypair.test_ui_verify_keypair_given_name("import-key")
         self.eucaUITester.base.test_ui_logout()
 
     def delete_keypair(self):
@@ -36,13 +37,16 @@ class testcase_keypair(testcase_base):
         print "=== runTest: Create Keypair Given Name ==="
         self.eucaUITester.base.test_ui_login()
         self.eucaUITester.keypair.test_ui_generate_keypair_given_name(keypair_name)
+        self.eucaUITester.keypair.test_ui_verify_keypair_given_name(keypair_name)
         self.eucaUITester.base.test_ui_logout()
 
     def create_keypair_given_name_then_delete_it(self, keypair_name='testkey-789'):
         print "=== runTest: Create Keypair Named testkey-789 then Delete It==="
         self.eucaUITester.base.test_ui_login()
         self.eucaUITester.keypair.test_ui_generate_keypair_given_name(keypair_name)
+        self.eucaUITester.keypair.test_ui_verify_keypair_given_name(keypair_name)
         self.eucaUITester.keypair.test_ui_delete_keypair_given_name(keypair_name)
+        self.eucaUITester.keypair.test_ui_verify_delete_keypair_given_name(keypair_name)
         self.eucaUITester.base.test_ui_logout()
 
 
