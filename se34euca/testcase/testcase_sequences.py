e440398c5eb6a6d173bd2a85bb406423f945e3dc
Kyo Lee <kyolee310@gmail.com>
2013-10-10 16:49:13 -0700

    Minor fix in volume seq at the moment

--- se34euca/testcase/testcase_sequences.py
+++ se34euca/testcase/testcase_sequences.py
@@ -117,14 +117,12 @@ class testcase_sequences(testcase_base):
 
     def volume_operations(self):
         sleep_time = 60
-        print "=== runTest: Instance Operations ==="
+        print "=== runTest: Volume Operations ==="
         self.eucaUITester.base.test_ui_login()
         self.eucaUITester.keypair.test_ui_import_keypair_given_name("import-key")
         self.eucaUITester.keypair.test_ui_verify_keypair_given_name("import-key")
         self.eucaUITester.keypair.test_ui_check_keypair_count("1")
-        self.eucaUITester.instance.test_ui_launch_instance_given_name_security_group_keypair("testinstance",
-                                                                                             "default",
-                                                                                          "import-key")
+        self.eucaUITester.instance.test_ui_launch_instance_given_name_security_group_keypair("testinstance", "default", "import-key")
         self.eucaUITester.volume.test_ui_create_volume()
         time.sleep(sleep_time)
         self.eucaUITester.volume.test_ui_check_volume_count("1")
